74dca41db9a5728d5a060e5145da9783b049dff8
gyunnybot <idjkda4034@gmail.com>
2024-12-16 14:23:40 +0900

    Create 009_7562.cpp

---
+++ BOJ/BarkingDog/09/009_7562.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+const int dy[] = { -2,-1,1,2,2,1,-1,-2 };
+const int dx[] = { 1,2,2,1,-1,-2,-2,-1 };
+int t, n, sy, sx, ey, ex, visited[301][301];
+
+void bfs(int y, int x) {
+    visited[y][x] = 1;
+
+    queue<pair<int, int>> q;
+
+    q.push({ y,x });
+
+    while (q.size()) {
+        pair<int, int> cur = q.front(); q.pop();
+
+        for (int i = 0; i < 8; i++) {
+            int ny = cur.first + dy[i];
+            int nx = cur.second + dx[i];
+
+            if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+
+            if (!visited[ny][nx]) {
+                visited[ny][nx] = visited[cur.first][cur.second] + 1;
+                q.push({ ny,nx });
+            }
+        }
+    }
+
+    return;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> t;
+
+    while (t--) {
+        fill(&visited[0][0], &visited[0][0] + 301 * 301, 0);
+
+        cin >> n >> sy >> sx >> ey >> ex;
+
+        bfs(sy, sx);
+
+        cout << visited[ey][ex] - 1 << '\n';
+    }
+
+    return 0;
+}
