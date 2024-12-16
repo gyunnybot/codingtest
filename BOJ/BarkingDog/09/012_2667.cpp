a0eb90887b45db9e2cfe65e2778892cdd17866a7
gyunnybot <idjkda4034@gmail.com>
2024-12-16 14:35:01 +0900

    Create 012_2667.cpp

---
+++ BOJ/BarkingDog/09/012_2667.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm> //sort
+using namespace std;
+
+const int dy[] = { -1,0,1,0 };
+const int dx[] = { 0,1,0,-1 };
+int n, ret;
+char a[30][30];
+bool visited[30][30];
+string s;
+vector<int> v;
+
+int bfs(int y, int x) {
+    visited[y][x] = true;
+
+    queue<pair<int, int>> q;
+    q.push({ y,x });
+
+    int ret = 1;
+
+    while (!q.empty()) {
+        pair<int, int> cur = q.front(); q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int ny = cur.first + dy[i];
+            int nx = cur.second + dx[i];
+
+            if (ny < 0 || ny >= n || nx < 0 || nx >= n) continue;
+
+            if (a[ny][nx] == '1' && !visited[ny][nx]) {
+                visited[ny][nx] = true;
+                q.push({ ny, nx });
+
+                ret++;
+            }
+        }
+    }
+
+    return ret;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+
+        for (int j = 0; j < n; j++) {
+            a[i][j] = s[j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (a[i][j] == '1' && !visited[i][j]) {
+                v.push_back(bfs(i, j));
+                ret++;
+            }
+        }
+    }
+
+    sort(v.begin(), v.end());
+
+    cout << ret << '\n';
+
+    for (int i : v) {
+        cout << i << '\n';
+    }
+
+    return 0;
+}
