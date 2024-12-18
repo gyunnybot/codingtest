d4dc827b8f0fd95341fc38b19d671c074ea63d2a
gyunnybot <idjkda4034@gmail.com>
2024-12-18 16:42:00 +0900

    Update 029_3197.cpp

--- BOJ/BarkingDog/09/029_3197.cpp
+++ BOJ/BarkingDog/09/029_3197.cpp
@@ -13,35 +13,6 @@ queue<pair<int, int>> swan_q, next_swan_q;
 vector<pair<int, int>> swan;
 string s;
 
-bool meet() {
-    while (!swan_q.empty()) {
-        pair<int, int> cur = swan_q.front(); swan_q.pop();
-
-        for (int i = 0; i < 4; i++) {
-            int ny = cur.first + dy[i];
-            int nx = cur.second + dx[i];
-
-            if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
-
-            if (!visited[ny][nx]) {
-                visited[ny][nx] = true;
-
-                if (a[ny][nx] == 'L') { //두 백조가 만났다면
-                    return true;
-                }
-                else if (a[ny][nx] == '.') {
-                    swan_q.push({ ny,nx }); //현재 백조가 실제로 다니는 경로(백조 또는 물)
-                }
-                else if (a[ny][nx] == 'X') {
-                    next_swan_q.push({ ny,nx }); //얼음이 녹은 후 백조가 다닐 수 있는 좌표
-                }
-            }
-        }
-    }
-
-    return false;
-}
-
 void melt() {
     while (!water.empty()) {
         pair<int, int> cur = water.front(); water.pop();
@@ -59,7 +30,7 @@ void melt() {
 
                 if (a[ny][nx] == 'X') {
                     a[ny][nx] = '.'; //얼음 녹이기
-                    next_water.push({ ny,nx }); //얼음이 녹은 후 백조가 다닐 수 있는 경로(물)
+                    next_water.push({ ny,nx }); //다음 melt에 녹을 물의 좌표 저장
                 }
             }
         }
@@ -68,9 +39,38 @@ void melt() {
     return;
 }
 
+bool meet() {
+    while (!swan_q.empty()) {
+        pair<int, int> cur = swan_q.front(); swan_q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int ny = cur.first + dy[i];
+            int nx = cur.second + dx[i];
+
+            if (ny < 0 || ny >= n || nx < 0 || nx >= m) continue;
+
+            if (!visited[ny][nx]) {
+                visited[ny][nx] = true;
+
+                if (a[ny][nx] == 'L') { //두 백조가 만났다면
+                    return true;
+                }
+                else if (a[ny][nx] == '.') {
+                    swan_q.push({ ny,nx }); //백조가 현재 다니는 경로(물)에 실시간으로 저장
+                }
+                else if (a[ny][nx] == 'X') {
+                    next_swan_q.push({ ny,nx }); //melt 이후 백조가 다닐 수 있는 경로
+                }
+            }
+        }
+    }
+
+    return false;
+}
+
 void bfs() {
     visited[swan[0].first][swan[0].second] = true;
-    swan_q.push(swan[0]); //현재 백조가 실제로 다니는 경로(백조 또는 물)
+    swan_q.push(swan[0]); //현재 백조가 움직임을 시작할 경로(물 또는 백조)
 
     while (true) {
         if (meet()) {
@@ -90,7 +90,7 @@ void bfs() {
 
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    cin.tie(NULL); cout.tie(NULL);
 
     cin >> n >> m;
 
@@ -101,7 +101,7 @@ int main() {
             a[i][j] = s[j];
 
             if (a[i][j] != 'X') {
-                water.push({ i,j }); //백조가 다닐 수 있는 경로(백조 또는 물)
+                water.push({ i,j }); //물 또는 백조의 좌표 저장
             }
 
             if (a[i][j] == 'L') {
