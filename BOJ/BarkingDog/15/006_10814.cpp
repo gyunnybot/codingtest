acc06f7f52adba26398e94560ca60ae506a87509
gyunnybot <idjkda4034@gmail.com>
2024-12-21 15:57:48 +0900

    Create 006_10814.cpp

---
+++ BOJ/BarkingDog/15/006_10814.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<algorithm> //stable_sort
+#include<vector>
+using namespace std;
+
+int n, age;
+string name;
+vector<pair<int, string>> v;
+
+bool cmp(pair<int, string> a, pair<int, string> b) {
+	return a.first < b.first;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> age >> name;
+		v.push_back({ age,name });
+	}
+
+	stable_sort(v.begin(), v.end(), cmp);
+
+	for (int i = 0; i < n; i++) {
+		cout << v[i].first << ' ' << v[i].second << '\n';
+	}
+
+	return 0;
+}
