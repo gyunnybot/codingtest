c6be3674be5652653ce453d06de2c605fb783f97
gyunnybot <idjkda4034@gmail.com>
2024-12-21 15:57:08 +0900

    Create 005_2910.cpp

---
+++ BOJ/BarkingDog/15/005_2910.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<map>
+#include<vector>
+#include<algorithm> //sort
+using namespace std;
+
+int n, c, temp;
+map<int, int> mp, mp2;
+vector<pair<int, int>> v;
+
+bool cmp(pair<int, int> a, pair<int, int>b) {
+	if (a.second == b.second) {
+		return mp2[a.first] < mp2[b.first];
+	}
+	else {
+		return a.second > b.second;
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> c;
+
+	for (int i = 0; i < n; i++) {
+		cin >> temp;
+
+		mp[temp]++; //값 - 횟수
+
+		if (!mp2[temp]) {
+			mp2[temp] = i + 1; //값 - 순서
+		}
+	}
+
+	for (pair<int, int> i : mp) {
+		v.push_back(i);
+	}
+
+	sort(v.begin(), v.end(), cmp);
+
+	for (pair<int, int> i : v) {
+		for (int j = 0; j < i.second; j++) {
+			cout << i.first << ' ';
+		}
+	}
+
+	return 0;
+}
