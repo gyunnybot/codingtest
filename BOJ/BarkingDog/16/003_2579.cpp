3ad89c177611523dd73cc91169dfa41b0661c8cf
gyunnybot <idjkda4034@gmail.com>
2024-12-22 13:13:07 +0900

    Create 003_2579.cpp

---
+++ BOJ/BarkingDog/16/003_2579.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int n, a[301], dp[301];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+
+	dp[0] = 0; //시작점은 계단에 포함되지 않는다
+	dp[1] = a[1];
+	dp[2] = a[2] + a[1];
+
+	for (int i = 3; i <= n; i++) {
+		dp[i] = a[i] + max(dp[i - 2], dp[i - 3] + a[i - 1]);
+	}
+	
+	cout << dp[n];
+
+	return 0;
+}
