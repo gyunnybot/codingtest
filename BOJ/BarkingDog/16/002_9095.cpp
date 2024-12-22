fcc8ee966c7e9efb835263028d02384834c5d364
gyunnybot <idjkda4034@gmail.com>
2024-12-22 12:52:57 +0900

    Create 002_9095.cpp

---
+++ BOJ/BarkingDog/16/002_9095.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int t, n, dp[11]; //n은 양수이며 11보다 작다
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	dp[0] = 0;
+	dp[1] = 1;
+	dp[2] = 2;
+	dp[3] = 4;
+
+	for (int i = 4; i <= 10; i++) {
+		dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+	}
+
+	cin >> t;
+
+	while (t--) {
+		cin >> n;
+		cout << dp[n] << '\n';
+	}
+
+	return 0;
+}
