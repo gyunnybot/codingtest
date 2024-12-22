40aefe09f60ce12e6346a9595b220bfba07395b9
gyunnybot <idjkda4034@gmail.com>
2024-12-22 12:27:37 +0900

    Create 001_1463_01.cpp

---
+++ BOJ/BarkingDog/16/001_1463_01.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int n, ret, visited[1000001];
+
+void bfs(int n) {
+	visited[n] = 1;
+
+	if (n == 1) {
+		ret = visited[n] - 1;
+		return;
+	}
+
+	queue<int> q;
+	q.push(n);
+
+	while (!q.empty()) {
+		int cur = q.front(); q.pop();
+
+		if (cur == 1) {
+			ret = visited[cur] - 1;
+			break;
+		}
+
+		if ((cur % 3) == 0 && !visited[cur / 3]) {
+			visited[cur / 3] = visited[cur] + 1;
+			q.push((cur / 3));
+		}
+
+		if ((cur % 2) == 0 && !visited[cur / 2]) {
+			visited[cur / 2] = visited[cur] + 1;
+			q.push((cur / 2));
+		}			
+
+		if (cur - 1 >= 1 && !visited[cur - 1]) {
+			visited[cur - 1] = visited[cur] + 1;
+			q.push(cur - 1);
+		}
+	}
+
+	return;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	bfs(n);
+
+	cout << ret;
+
+	return 0;
+}
