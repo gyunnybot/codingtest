4cf2842e177bde9e9ba5b42f93c4d6fd9e5b7751
gyunnybot <idjkda4034@gmail.com>
2024-12-24 21:50:33 +0900

    Create 014_11053.cpp

---
+++ BOJ/BarkingDog/16/014_11053.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int n, a[1001], dp[1001], ret;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	fill(&dp[0], &dp[0] + 1001, 1); //본인을 시작점으로 둔다면 길이는 항상 1 이상이다
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			if (a[i] > a[j]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++) { //수열 A를 이루고 있는 Ai가 주어진다. (1 ≤ Ai ≤ 1,000)
+		ret = max(ret, dp[i]);
+	}
+
+	cout << ret;
+
+	return 0;
+}
