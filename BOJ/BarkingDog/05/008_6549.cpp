9fbb8c4c76da46800fd6f9afd54024b88aa8cd93
gyunnybot <idjkda4034@gmail.com>
2024-12-24 14:47:39 +0900

    Create 008_6549.cpp

---
+++ BOJ/BarkingDog/05/008_6549.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+typedef long long ll;
+
+int n, h, idx;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	while (true) {
+		cin >> n;
+
+		if (n == 0) break;
+
+		stack<pair<ll, ll>> stk;
+		ll ret = 0;
+
+		for (int i = 0; i < n; i++) {
+			cin >> h;
+
+			idx = i;
+
+			while (!stk.empty() && stk.top().first >= h) { //입력된 높이가 직전 높이보다 작다면 중간 검사 실행
+				//바로 이전 위치부터 역으로 만들 수 있는 직사각형을 전부 조사 후 최대값 갱신
+				ret = max(ret, (i - stk.top().second) * stk.top().first);
+
+				idx = stk.top().second;
+
+				stk.pop();
+			}
+
+			stk.push({ h,idx });
+		}
+
+		while (!stk.empty()) { //마지막 기둥 검사
+			ret = max(ret, (n - stk.top().second) * stk.top().first);
+
+			stk.pop();
+		}
+
+		cout << ret << '\n';
+	}
+
+	return 0;
+}
