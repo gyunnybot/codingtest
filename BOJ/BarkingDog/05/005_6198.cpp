6e2b2bcfb12531a71b6796bef2e422aad871b30c
gyunnybot <idjkda4034@gmail.com>
2024-12-24 14:13:30 +0900

    Create 005_6198.cpp

---
+++ BOJ/BarkingDog/05/005_6198.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+typedef long long ll;
+
+int n, height;
+stack<int> stk; //모든 빌딩은 일렬로 서 있고 오른쪽으로'만' 볼 수 있다 = stack 활용
+ll ret; //최대 64억
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	//monotone stack
+	while (n--) {
+		cin >> height;
+
+		while (!stk.empty() && stk.top() <= height) {
+			stk.pop();
+		}
+
+		//본인이 내려다볼 수 있는 빌딩 수의 총합 = 본인을 내려다볼 수 있는 빌딩 수의 총합
+		ret += stk.size();
+
+		stk.push(height);
+	}
+
+	cout << ret;
+
+	return 0;
+}
