f050686362cb22acf14fa632c4a82f551f7c45c5
gyunnybot <idjkda4034@gmail.com>
2024-12-30 21:41:50 +0900

    Update 017_14890.cpp

--- BOJ/BarkingDog/13/017_14890.cpp
+++ BOJ/BarkingDog/13/017_14890.cpp
@@ -3,7 +3,7 @@
 #include<climits> //INT_MAX
 using namespace std;
 
-int n, l, a[101][101], b[101][101], ret;
+int n, L, a[101][101], b[101][101], ret;
 
 void go(int a[101][101]) {
     for (int i = 0; i < n; i++) {
@@ -14,17 +14,17 @@ void go(int a[101][101]) {
             if (a[i][j] == a[i][j + 1]) {
                 cnt++; //본인과 같은 높이의 블록의 수 증가
             }
-            else if (a[i][j] == a[i][j + 1] - 1 && cnt >= l) {
+            else if (a[i][j] == a[i][j + 1] - 1 && cnt >= L) {
                 cnt = 1; //높아진 블록에서 새로운 출발
             }
             else if (a[i][j] - 1 == a[i][j + 1] && cnt >= 0) {
-                cnt = -l + 1; //l을 온전히 품을 때까지 cnt 조정
+                cnt = -L + 1; //l을 온전히 품을 때까지 cnt 조정
             }
             else {
                 //올라가는 경사로의 길이만큼의 cnt가 없거나 (두 번째 if 문)
                 //내려가는 경사로가 깔릴만큼의 배열의 길이가 없어 cnt = 0이 안된다면 (세 번째 if 문) break
                 break;
-            }            
+            }
         }
 
         //cnt < 0일 경우 내려가는 경사로가 범위를 벗어난다
@@ -40,7 +40,7 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
-    cin >> n >> l;
+    cin >> n >> L;
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
