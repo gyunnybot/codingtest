a737087e5c60ef2b228a6f00e6b4b230d35d9843
gyunnybot <idjkda4034@gmail.com>
2024-12-31 21:09:33 +0900

    Update 003_1874.cpp

--- BOJ/BarkingDog/05/003_1874.cpp
+++ BOJ/BarkingDog/05/003_1874.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 int n, temp;
+int cnt = 1;
+bool flag;
 string ret;
 stack<int> stk;
 
@@ -12,20 +14,18 @@ int main() {
 
 	cin >> n;
 
-	int cnt = 1; //부호 출력을 위한 cnt 변수
-	bool flag = false;
-
 	while (n--) {
 		cin >> temp;
 
 		while (cnt <= temp) {
 			ret += "+\n";
+
 			stk.push(cnt);
 			cnt++;
 		}
 
 		if (stk.top() != temp) {
-			flag = true; //도중에 스택 수열이 성립되지 않아도 일단 입력받는다
+			flag = true; //도중에 수열이 성립하지 않아도 일단 입력받는다
 		}
 		else {
 			ret += "-\n";
