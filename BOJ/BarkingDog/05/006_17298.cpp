4037e117200658c6faf8d5d2a077803a3bc1928c
gyunnybot <idjkda4034@gmail.com>
2025-01-02 14:36:26 +0900

    Update 006_17298.cpp

--- BOJ/BarkingDog/05/006_17298.cpp
+++ BOJ/BarkingDog/05/006_17298.cpp
@@ -1,10 +1,9 @@
 #include<iostream>
 #include<stack>
-#include<vector>
 using namespace std;
 
-int n, temp, arr[1000001];
 stack<int> stk;
+int n, a[1000001], ret[1000001];
 
 int main() {
 	ios_base::sync_with_stdio(false);
@@ -12,17 +11,18 @@ int main() {
 
 	cin >> n;
 
-	vector<int> ret(n, -1);
+	fill(&ret[0], &ret[0] + n, -1);
 
 	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
+		cin >> a[i];
 
-		while (!stk.empty() && arr[stk.top()] < arr[i]) {
-			ret[stk.top()] = arr[i];
+		//Ai의 오큰수는 오른쪽에 있으면서(stack 활용) Ai보다 큰 수 중에서 가장 왼쪽에 있는 수(stk.top())를 의미한다
+		while (!stk.empty() && a[stk.top()] < a[i]) {
+			ret[stk.top()] = a[i];
 			stk.pop();
 		}
-
-		stk.push(i); //idx push
+		
+		stk.push(i);
 	}
 
 	for (int i = 0; i < n; i++) {
