be39f09f52166721b6a9149bf35fd7ee462cd0c4
gyunnybot <idjkda4034@gmail.com>
2025-01-02 14:40:16 +0900

    Update 007_3015.cpp

--- BOJ/BarkingDog/05/007_3015.cpp
+++ BOJ/BarkingDog/05/007_3015.cpp
@@ -16,20 +16,20 @@ int main(void) {
 	while (n--) {
 		cin >> h; //현재 키 입력
 
-		int cnt = 1; //본인 포함 같은 키인 사람들의 수
+		int cnt = 1; //본인 포함 같은 키인 사람들의 수 계산하기
 
-		while (!stk.empty() && stk.top().first <= h) { //스택에 저장된 키보다 크다면(직전 사람부터 역순으로 계산)
-			ret += stk.top().second; //직전 사람들의 cnt만큼의 사람들을 현재 사람도 볼 수 있으므로 가짓 수 추가
+		while (!stk.empty() && stk.top().first <= h) { //스택 안의 사람보다 크다면(가장 직전 사람부터 역순으로 계산)
+			ret += stk.top().second; //직전 사람 cnt만큼의 사람들을 현재 사람 또한 볼 수 있으므로 결과 가짓 수 추가
 
-			if (stk.top().first == h) { //키가 정확히 같다면
-				cnt += stk.top().second; //현재 본인의 cnt에 스택 cnt를 추가하여 갱신
+			if (stk.top().first == h) { //만약 키가 정확히 같다면
+				cnt += stk.top().second; //스택 cnt를 추가하여 현재 본인의 cnt 갱신
 			}
 
 			stk.pop(); //더 이전 사람과의 비교를 위해 pop
 		}
 
 		if (!stk.empty()) {
-			ret++;
+			ret++; //스택 안의 사람이 더 큰 경우 마지막으로 결과 가짓 수 추가
 		}
 
 		stk.push({ h, cnt }); //현재 키, 본인 포함 같은 키인 사람들의 수
