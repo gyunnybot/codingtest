a1ac01ef0834f8e89ee55cc953459331005fcedc
gyunnybot <idjkda4034@gmail.com>
2025-01-02 23:33:21 +0900

    Update 005_11559.cpp

--- BOJ/BarkingDog/13/005_11559.cpp
+++ BOJ/BarkingDog/13/005_11559.cpp
@@ -5,7 +5,7 @@ using namespace std;
 const int dy[] = { -1,0,1,0 };
 const int dx[] = { 0,1,0,-1 };
 
-vector<pair<int, int>> blocks, ret_blocks;
+vector<pair<int, int>> cur_blocks, ret_blocks;
 string s;
 char a[14][8];
 bool visited[14][8];
@@ -14,7 +14,7 @@ int ret;
 void dfs(int y, int x, char color) {
     visited[y][x] = 1;
 
-    blocks.push_back({ y,x });
+    cur_blocks.push_back({ y,x });
 
     for (int i = 0; i < 4; i++) {
         int ny = y + dy[i];
@@ -39,31 +39,31 @@ void puyo() {
         for (int i = 11; i >= 0; i--) {
             for (int j = 0; j < 6; j++) {
                 if (a[i][j] != '.' && !visited[i][j]) {
-                    dfs(i, j, a[i][j]); //폭발 가능한 블록 덩어리 찾기
+                    dfs(i, j, a[i][j]); //터뜨릴 블록 덩어리 찾기
 
-                    if (blocks.size() >= 4) {
-                        flag = true; //폭발 가능
+                    if (cur_blocks.size() >= 4) {
+                        flag = true;
 
-                        //폭발 가능한 블록들을 ret_blocks에 저장하기
-                        for (pair<int, int> pos : blocks) {
+                        //터뜨릴 블록들을 ret_blocks에 저장
+                        for (pair<int, int> pos : cur_blocks) {
                             ret_blocks.push_back(pos);
                         }
                     }
 
-                    blocks.clear(); //블록 덩어리 초기화
+                    cur_blocks.clear(); //터뜨릴 블록 덩어리 초기화
                 }
             }
         }
 
-        //ret_blocks에 저장된 블록들을 '.'으로 바꾸기(폭발)
+        //블록 터뜨리기 = ret_blocks에 저장된 블록들을 '.'으로 바꾸기
         for (int i = 0; i < ret_blocks.size(); i++) {
             a[ret_blocks[i].first][ret_blocks[i].second] = '.';
         }
 
-        ret_blocks.clear();
+        ret_blocks.clear(); //터뜨릴 전체 블록 모음 초기화
 
-        //한 칸씩 아래로 내리기
-        for (int i = 11; i >= 0; i--) {
+        //뿌요는 중력의 영향을 받아 아래에 바닥이나 다른 뿌요가 나올 때까지 아래로 떨어진다
+        for (int i = 11; i >= 0; i--) { //맨 아래 행부터 계산
             for (int j = 0; j < 6; j++) {
                 if (a[i][j] == '.') continue;
 
@@ -73,16 +73,16 @@ void puyo() {
                     if (tmp == 11 || a[tmp + 1][j] != '.') { //바닥에 도달했거나 블록이 막고 있다면 break
                         break;
                     }
-                    else {              
-                        a[tmp + 1][j] = a[tmp][j]; 
+                    else {
+                        a[tmp + 1][j] = a[tmp][j];
                         a[tmp][j] = '.';
                         tmp++;
-                    }                    
+                    }
                 }
             }
         }
 
-        if (flag) { //폭발이 발생했다면 연쇄 추가 후 while문 다시 진행
+        if (flag) { //연쇄가 발생했다면 전체 while문 다시 실행하여 추가 연쇄 확인
             ret++;
         }
         else {
