d936b517ee2a40dec134c893ca38399e879530e2
gyunnybot <idjkda4034@gmail.com>
2025-01-04 19:04:29 +0900

    Create 003_5430.cpp

---
+++ BOJ/BarkingDog/07/003_5430.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+#include<deque>
+#include<algorithm> //reverse
+using namespace std;
+
+int t, n;
+string p, x;
+
+deque<string> split(string input, string delimiter) {
+	deque<string> result;
+
+	int start = 0;
+	int end = input.find(delimiter);
+
+	while (end != string::npos) {
+		result.push_back(input.substr(start, end - start));
+		start = end + delimiter.size();
+		end = input.find(delimiter, start);
+	}
+
+	result.push_back(input.substr(start));
+
+	return result;
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> t;
+
+	while (t--) {
+		cin >> p >> n >> x;
+
+		string test_str = "";
+		deque<string> ret_str;
+
+		bool flag = false;
+		bool reversed = false;
+
+		for (int i = 1; i < x.size() - 1; i++) { //괄호 제거 후 test_str에 저장
+			test_str += x[i];
+		}
+
+		if (!test_str.empty()) {
+			ret_str = split(test_str, ","); //","를 기준으로 문자열 split 후 ret_str에 저장
+		}
+
+		for (char c : p) {
+			if (c == 'R') {
+				reversed = !reversed; //reverse 상태 변경
+			}
+			else if (c == 'D') {
+				if (ret_str.empty()) {
+					flag = true; //배열에 값이 없는데 D가 호출될 경우
+					break; //더 이상 진행할 이유가 없음
+				}
+				else { //반전 상태에 따라 pop의 위치 결정
+					if (reversed) {
+						ret_str.pop_back();
+					}
+					else {
+						ret_str.pop_front();
+					}
+				}
+			}
+		}
+
+		if (reversed) reverse(ret_str.begin(), ret_str.end()); //최종 출력을 위한 reverse 연산
+
+		if (flag) { //배열에 값이 없는데 D가 호출될 경우
+			cout << "error" << '\n';
+		}
+		else { //배열에 값이 없는데 R이 호출될 경우 + 값이 있는 경우에 D, R이 호출될 경우
+			if (ret_str.empty()) { //1. 배열에 값이 없는데 R이 호출될 경우
+				cout << "[]" << '\n';
+			}
+			else { //2, 3. 배열의 값이 남아있을 때의 D, R 연산 결과 출력
+				cout << '[';
+
+				cout << ret_str.front();
+				ret_str.pop_front();
+
+				while (!ret_str.empty()) {
+					cout << ',' << ret_str.front();
+					ret_str.pop_front();
+				}
+
+				cout << ']' << '\n';
+			}
+		}
+	}
+
+	return 0;
+}
