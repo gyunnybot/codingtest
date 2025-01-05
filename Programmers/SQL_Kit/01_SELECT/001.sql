67978991e377f466682a26424a92714919c7e556
gyunnybot <idjkda4034@gmail.com>
2025-01-05 11:29:32 +0900

    Create sql

--- Programmers/SQL_Kit/01_SELECT/001.sql
+++ Programmers/SQL_Kit/01_SELECT/001.sql
@@ -1,3 +1,5 @@
-SELECT ROUND(AVG(DAILY_FEE),0) AS AVERAGE_FEE
+SELECT ROUND(AVG(DAILY_FEE), 0) AS AVERAGE_FEE
 FROM CAR_RENTAL_COMPANY_CAR 
 WHERE CAR_TYPE = 'SUV';
+
+-- ROUND(num, i) : 숫자 값 num을 소수 n번째 자릿수까지 반올림하여 출력. 음수를 사용하면 정수 자릿수 또한 제한할 수 있다
--- Programmers/SQL_Kit/01_SELECT/002.sql
+++ Programmers/SQL_Kit/01_SELECT/002.sql
@@ -1,6 +1,22 @@
 SELECT FIRST_HALF.FLAVOR
-FROM FIRST_HALF LEFT JOIN ICECREAM_INFO
+FROM FIRST_HALF LEFT OUTER JOIN ICECREAM_INFO
 ON FIRST_HALF.FLAVOR = ICECREAM_INFO.FLAVOR
 WHERE FIRST_HALF.TOTAL_ORDER > 3000
 AND ICECREAM_INFO.INGREDIENT_TYPE = 'fruit_based'
 ORDER BY FIRST_HALF.TOTAL_ORDER DESC;
+
+/*
+외부 조인: 조건 컬럼이 중복되어 출력됨. orders 레코드는 customer에 상관없이(null 처리) 무조건 출력
+select *
+from orders left join customer
+on orders.custid = customer.custid;
+
+내부 조인: 조건 컬럼이 중복되어 출력됨. 조건에 맞는 레코드만 출력된다
+select *
+from orders, customer
+where orders.custid = customer.custid;
+
+자연 조인: 겹치는 컬럼이 반드시 하나여야 한다. 겹치는 컬럼은 한 번만 출력됨
+select *
+from orders natural join customer;
+*/
