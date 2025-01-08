eea5b80c89e8d311af7f28d24db4d9fbebb55545
gyunnybot <idjkda4034@gmail.com>
2025-01-08 16:23:48 +0900

    commit

--- Programmers/SQL_Kit/01_SELECT/002.sql
+++ Programmers/SQL_Kit/01_SELECT/002.sql
@@ -6,17 +6,17 @@ AND ICECREAM_INFO.INGREDIENT_TYPE = 'fruit_based'
 ORDER BY FIRST_HALF.TOTAL_ORDER DESC;
 
 /*
-외부 조인: 조건 컬럼이 중복되어 출력됨. orders 레코드는 customer에 상관없이(null 처리) 무조건 출력
+외부 조인: 조건 컬럼이 중복되어 출력. orders에 속한 레코드는 무조건 출력된다
 select *
 from orders left join customer
 on orders.custid = customer.custid;
 
-내부 조인: 조건 컬럼이 중복되어 출력됨. 조건에 맞는 레코드만 출력된다
+내부 조인: 조건 컬럼이 중복되어 출력. 조건에 맞는 레코드만 출력된다
 select *
 from orders, customer
 where orders.custid = customer.custid;
 
-자연 조인: 겹치는 컬럼이 반드시 하나여야 한다. 겹치는 컬럼은 한 번만 출력됨
+자연 조인: 겹치는 컬럼이 반드시 하나여야 한다! 겹치는 컬럼이 한 번만 출력된다
 select *
 from orders natural join customer;
 */
