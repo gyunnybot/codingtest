2a1f8f0f3630de5bec8e3bdcc9bdc7abbe45064b
gyunnybot <idjkda4034@gmail.com>
2025-01-15 14:08:27 +0900

    Update 011_2193.cpp

--- BOJ/BarkingDog/0x10/011_2193.cpp
+++ BOJ/BarkingDog/0x10/011_2193.cpp
@@ -13,8 +13,9 @@ int main() {
 
     dp[0] = 0;
     dp[1] = 1;
+    dp[2] = 1;
 
-    for (int i = 2; i <= n; i++) {
+    for (int i = 3; i <= n; i++) {
         dp[i] = dp[i - 1] + dp[i - 2];
     }
 
