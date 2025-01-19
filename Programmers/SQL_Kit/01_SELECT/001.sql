916e80ce0df379931eaae6e6f6504beb449b361b
gyunnybot <idjkda4034@gmail.com>
2025-01-19 11:28:14 +0900

    update select sql

--- Programmers/SQL_Kit/01_SELECT/001.sql
+++ Programmers/SQL_Kit/01_SELECT/001.sql
@@ -2,4 +2,4 @@ SELECT ROUND(AVG(DAILY_FEE), 0) AS AVERAGE_FEE
 FROM CAR_RENTAL_COMPANY_CAR
 WHERE CAR_TYPE = 'SUV';
 
--- ROUND(num, i) : 숫자 num(일반적으로 집계함수의 반환 값)을 소수 i번째 자릿수까지 반올림하여 출력
+-- ROUND(숫자, N) : 반올림하여 소수 N번째 자리수까지 해당 숫자 표현
--- Programmers/SQL_Kit/01_SELECT/002.sql
+++ Programmers/SQL_Kit/01_SELECT/002.sql
@@ -1,34 +1,10 @@
-SELECT FIRST_HALF.FLAVOR
-FROM FIRST_HALF LEFT OUTER JOIN ICECREAM_INFO
-ON FIRST_HALF.FLAVOR = ICECREAM_INFO.FLAVOR
-WHERE FIRST_HALF.TOTAL_ORDER > 3000
-AND ICECREAM_INFO.INGREDIENT_TYPE = 'fruit_based'
-ORDER BY FIRST_HALF.TOTAL_ORDER DESC;
+SELECT MEMBER_ID, MEMBER_NAME, GENDER, DATE_FORMAT(DATE_OF_BIRTH, '%Y-%m-%d') AS DATE_OF_BIRTH
+FROM MEMBER_PROFILE
+WHERE MONTH(DATE_OF_BIRTH) = 3
+AND GENDER = 'W'
+AND TLNO IS NOT NULL
+ORDER BY MEMBER_ID;
 
-/*
-외부 조인:
-SELECT *를 하면 조건 컬럼이 중복되어 출력
-orders에 속한 레코드는 무조건 출력되며 customer의 컬럼값은 null로 대체된다
-
-SELECT *
-FROM orders LEFT OUTER JOIN customer
-ON orders.custid = customer.custid;
----------------------------------------------------
-
-내부 조인:
-SELECT *를 하면 조건 컬럼이 중복되어 출력
-조건에 맞는 레코드만 출력된다
-
-SELECT *
-FROM orders, customer
-WHERE orders.custid = customer.custid;
----------------------------------------------------
-
-자연 조인:
-조건(겹치는) 컬럼이 반드시 하나여야 한다!
-SELECT *를 해도 조건 컬럼이 중복되지 않고 한 번만 출력된다
-
-SELECT *
-FROM orders NATURAL JOIN customer;
----------------------------------------------------
-*/
+-- DATE_FORMAT(DATE 타입의 컬럼, '나타낼 형식')
+-- MONTH(DATE 타입 컬럼) : DATE 타입에서 '월'만 추출
+-- 속성 IS NOT NULL : 속성값이 NULL이 아닌 레코드만 추출
