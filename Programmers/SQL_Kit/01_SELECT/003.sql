bfeb1ccc5ee8e27b74f42390dcead68f616f0b39
gyunnybot <idjkda4034@gmail.com>
2025-01-19 12:06:07 +0900

    Update 003.sql

--- Programmers/SQL_Kit/01_SELECT/003.sql
+++ Programmers/SQL_Kit/01_SELECT/003.sql
@@ -5,3 +5,35 @@ WHERE I.ADDRESS LIKE '서울%'
 AND R.REVIEW_SCORE IS NOT NULL
 GROUP BY I.REST_ID, I.REST_NAME, I.FOOD_TYPE, I.FAVORITES, I.ADDRESS
 ORDER BY SCORE DESC, I.FAVORITES DESC;
+
+/*
+외부 조인:
+조건 컬럼이 중복되어 출력
+orders에 속한 레코드는 무조건 출력되며, orders와 조건이 맞지 않는 customer의 컬럼값은 null로 대체되어 출력
+
+SELECT *
+FROM orders LEFT OUTER JOIN customer
+ON orders.custid = customer.custid;
+---------------------------------------------------
+
+내부 조인:
+조건 컬럼이 중복되어 출력
+조건에 맞는 레코드만 선택적으로 출력된다
+
+SELECT *
+FROM orders INNER JOIN customer
+ON orders.custid = customer.custid;
+
+SELECT *
+FROM orders, customer
+WHERE orders.custid = customer.custid;
+---------------------------------------------------
+
+자연 조인:
+조건(겹치는) 컬럼이 반드시 하나여야 한다!
+조건 컬럼이 중복되지 않고 한 번만 출력된다
+
+SELECT *
+FROM orders NATURAL JOIN customer;
+---------------------------------------------------
+*/
