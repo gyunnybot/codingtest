885d692e0d65d84e65a32561eb01804f0c4ecfd1
gyunnybot <idjkda4034@gmail.com>
2025-01-30 19:18:40 +0900

    commit

---
+++ BOJ/BarkingDog/0x15/008_19583.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<unordered_map>
+using namespace std;
+
+unordered_map<string, int> ump;
+string s, e, q, t, name;
+int ret;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> s >> e >> q;
+
+    while (cin >> t >> name) {
+        if (t <= s) {
+            ump[name] = 1;
+        }
+        else if (e <= t && t <= q) {
+            if (ump[name] == 1) {
+                ump[name] = 2;
+                ret++;
+            }
+        }
+    }
+
+    cout << ret;
+
+    return 0;
+}
---
+++ BOJ/BarkingDog/0x17/004_2075.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<queue> //priority_queue
+using namespace std;
+
+int n, temp;
+priority_queue<int, vector<int>, greater<int>> pq; //최소 힙
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    cin >> n;
+
+    for (int i = 0; i < n * n; i++) {
+        cin >> temp;
+        pq.push(temp);
+
+        if (pq.size() > n) {
+            pq.pop();
+        }
+    }
+    
+    cout << pq.top();
+
+    return 0;
+}
