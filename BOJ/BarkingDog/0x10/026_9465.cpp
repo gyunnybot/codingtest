ff50427cf71e4c49d1eab5612683e8d46dc63dc2
gyunnybot <idjkda4034@gmail.com>
2025-02-23 22:08:17 +0900

    commit

---
+++ BOJ/BarkingDog/0x10/026_9465.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int t, n, a[2][100001], dp[2][100001];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> t;
+
+    while (t--) {
+        cin >> n;
+
+        fill(&a[0][0], &a[0][0] + 2 * 100001, 0);
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 1; j <= n; j++) {
+                cin >> a[i][j];
+            }
+        }
+
+        dp[0][0] = dp[0][1] = 0;
+        dp[0][1] = a[0][1];
+        dp[1][1] = a[1][1];
+
+        for (int i = 2; i <= n; i++) {
+            dp[0][i] = max(dp[1][i - 1], dp[1][i - 2]) + a[0][i];
+            dp[1][i] = max(dp[0][i - 1], dp[0][i - 2]) + a[1][i];
+        }
+
+        cout << max(dp[0][n], dp[1][n]) << '\n';
+    }
+
+    return 0;
+}
---
+++ BOJ/BarkingDog/0x10/031_4883.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<climits> //INT_MAX
+using namespace std;
+
+int n, a[100001][3], dp[100001][3];
+int t = 1;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    while (true) {
+        cin >> n;
+
+        if (n == 0) break;
+
+        fill(&a[0][0], &a[0][0] + 100001 * 3, 0);
+        fill(&dp[0][0], &dp[0][0] + 100001 * 3, 0);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 3; j++) {
+                cin >> a[i][j];
+            }
+        }
+
+        dp[0][0] = INT_MAX, dp[0][1] = a[0][1], dp[0][2] = a[0][1] + a[0][2];
+
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = min(dp[i - 1][0], dp[i - 1][1]) + a[i][0];
+            dp[i][1] = min(min(min(dp[i - 1][0], dp[i - 1][1]), dp[i - 1][2]), dp[i][0]) + a[i][1];
+            dp[i][2] = min(min(dp[i - 1][1], dp[i - 1][2]), dp[i][1]) + a[i][2];
+        }
+
+        cout << t << ". " << dp[n - 1][1] << '\n';
+
+        t++;
+    }
+
+    return 0;
+}
