8f44a09b8109a3b46db5ec633234bdcb2cbb7d3a
gyunnybot <idjkda4034@gmail.com>
2025-02-27 22:17:42 +0900

    Update 002_18808.cpp

--- BOJ/BarkingDog/0x0D/002_18808.cpp
+++ BOJ/BarkingDog/0x0D/002_18808.cpp
@@ -4,97 +4,103 @@ using namespace std;
 int n, m, k, r, c, ret, a[42][42], paper[12][12];
 
 void rotate() {
-	int temp[12][12];
+    int temp[12][12];
 
-	for (int i = 0; i < r; i++) {
-		for (int j = 0; j < c; j++) {
-			temp[j][r - i - 1] = paper[i][j]; //시계 방향
-			//temp[c - j - 1][i] = paper[i][j]; //반시계 방향
-		}
-	}
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            temp[j][r - i - 1] = paper[i][j]; //clock wise
+            //temp[c - j - 1][i] = paper[i][j]; //counter-clock wise
+        }
+    }
 
-	for (int i = 0; i < r; i++) {
-		for (int j = 0; j < c; j++) {
-			paper[i][j] = 0;
-		}
-	}
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            paper[i][j] = 0;
+        }
+    }
 
-	for (int i = 0; i < c; i++) {
-		for (int j = 0; j < r; j++) {
-			paper[i][j] = temp[i][j];
-		}
-	}
+    for (int i = 0; i < c; i++) {
+        for (int j = 0; j < r; j++) {
+            paper[i][j] = temp[i][j];
+        }
+    }
 
-	swap(r, c); //swap : 참조자 연산(call by reference). 행과 열 바꾸기
+    swap(r, c);
 
-	return;
+    return;
 }
 
 bool postable(int y, int x) {
-	for (int i = 0; i < r; i++) {
-		for (int j = 0; j < c; j++) {
-			if (a[y + i][x + j] == 1 && paper[i][j] == 1) {
-				return false;
-			}
-		}
-	}
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            if (a[y + i][x + j] == 1 && paper[i][j] == 1) {
+                return false;
+            }
+        }
+    }
 
-	return true;
+    return true;
+}
+
+void post(int y, int x) {
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            if (paper[i][j] == 1) {
+                a[y + i][x + j] = 1;
+            }
+        }
+    }
+
+    return;
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n >> m >> k;
+    cin >> n >> m >> k;
 
-	while (k--) {
-		cin >> r >> c;
+    while (k--) {
+        fill(&paper[0][0], &paper[0][0] + 12 * 12, 0);
 
-		fill(&paper[0][0], &paper[0][0] + 12 * 12, 0);
+        cin >> r >> c;
 
-		for (int i = 0; i < r; i++) {
-			for (int j = 0; j < c; j++) {
-				cin >> paper[i][j];
-			}
-		}
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                cin >> paper[i][j];
+            }
+        }
 
-		bool flag = false;
+        bool flag = false;
 
-		for (int dir = 0; dir < 4; dir++) {
-			for (int y = 0; y <= n - r; y++) {
-				for (int x = 0; x <= m - c; x++) {
-					if (postable(y, x)) { //붙일 수 있는지 검사
-						for (int i = 0; i < r; i++) { //검사 통과! R x C 사이즈의 스티커 붙이기
-							for (int j = 0; j < c; j++) {
-								if (paper[i][j] == 1) {
-									a[y + i][x + j] = 1;
-								}
-							}
-						}
+        for (int dir = 0; dir < 4; dir++) {
+            for (int y = 0; y <= n - r; y++) {
+                for (int x = 0; x <= m - c; x++) {
+                    if (postable(y, x)) {                      
+                        post(y, x);
 
-						flag = true;
+                        flag = true;
 
-						break;
-					}
-				}
+                        break;
+                    }
+                }
 
-				if (flag) break;
-			}
+                if (flag) break;
+            }
 
-			if (flag) break;
+            if (flag) break;
 
-			rotate();
-		}
-	}
+            rotate();
+        }
+    }
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			ret += a[i][j];
-		}
-	}
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            ret += a[i][j];
+        }
+    }
 
-	cout << ret;
+    cout << ret;
 
-	return 0;
+    return 0;
 }
