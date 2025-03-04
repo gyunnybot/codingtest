8b9ea2b6e9d05c64a1df5485a4674dd3edfe2c2f
gyunnybot <idjkda4034@gmail.com>
2025-03-04 19:10:40 +0900

    Create 001_1717.cpp

---
+++ BOJ/BarkingDog/Appendix_D/001_1717.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> p(1000001, -1);
+int n, m, q, a, b;
+
+int find(int x) {
+	if (p[x] < 0) {
+		return x;
+	}
+	else {
+		p[x] = find(p[x]);
+		return p[x];
+	}
+}
+
+bool uni(int u, int v) {
+	u = find(u);
+	v = find(v);
+
+	if (u == v) {
+		return false;
+	}
+
+	if (p[u] > p[v]) { //v의 랭크가 더 큰 경우
+		swap(u, v);
+	}
+
+	if (p[u] == p[v]) {
+		p[u]--;
+	}
+
+	p[v] = u; //v를 u의 자식으로 만든다  
+
+	return true;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> m;
+
+	while (m--) {
+		cin >> q >> a >> b;
+
+		if (q == 0) {
+			uni(a, b);
+		}
+		else {
+			if (find(a) == find(b)) {
+				cout << "YES\n";
+			}
+			else {
+				cout << "NO\n";
+			}
+		}
+	}
+
+	return 0;
+}
