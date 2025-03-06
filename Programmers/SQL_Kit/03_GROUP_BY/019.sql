da1f22ab4eec385211ca32a628de65a61712573d
gyunnybot <idjkda4034@gmail.com>
2025-03-06 22:03:40 +0900

    UPDATE

--- Programmers/SQL_Kit/03_GROUP_BY/019.sql
+++ Programmers/SQL_Kit/03_GROUP_BY/019.sql
@@ -1,14 +1,12 @@
-SELECT HR_EMPLOYEES.EMP_NO, HR_EMPLOYEES.EMP_NAME, CASE
-                                                        WHEN AVG(HR_GRADE.SCORE) >= 96 THEN 'S'
+SELECT HR_EMPLOYEES.EMP_NO, HR_EMPLOYEES.EMP_NAME, CASE WHEN AVG(HR_GRADE.SCORE) >= 96 THEN 'S'
                                                         WHEN AVG(HR_GRADE.SCORE) >= 90 THEN 'A'
                                                         WHEN AVG(HR_GRADE.SCORE) >= 80 THEN 'B'
-                                                        ELSE 'C'
+                                                        ELSE 'C'                                                        
                                                     END AS GRADE,
-                                                    CASE
-                                                        WHEN AVG(HR_GRADE.SCORE) >= 96 THEN HR_EMPLOYEES.SAL * 0.2
-                                                        WHEN AVG(HR_GRADE.SCORE) >= 90 THEN HR_EMPLOYEES.SAL * 0.15
-                                                        WHEN AVG(HR_GRADE.SCORE) >= 80 THEN HR_EMPLOYEES.SAL * 0.1
-                                                        ELSE 0
+                                                    CASE WHEN AVG(HR_GRADE.SCORE) >= 96 THEN HR_EMPLOYEES.SAL * 0.2
+                                                         WHEN AVG(HR_GRADE.SCORE) >= 90 THEN HR_EMPLOYEES.SAL * 0.15
+                                                         WHEN AVG(HR_GRADE.SCORE) >= 80 THEN HR_EMPLOYEES.SAL * 0.1
+                                                         ELSE 0                                                        
                                                     END AS BONUS
 FROM HR_EMPLOYEES LEFT OUTER JOIN HR_GRADE
 ON HR_EMPLOYEES.EMP_NO = HR_GRADE.EMP_NO
