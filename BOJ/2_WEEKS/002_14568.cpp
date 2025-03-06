ce89eb7d986d4fbb31901e153fbf94d8b776cfbe
gyunnybot <idjkda4034@gmail.com>
2025-03-06 20:48:05 +0900

    update

--- BOJ/2_WEEKS/002_14568.cpp
+++ BOJ/2_WEEKS/002_14568.cpp
@@ -10,7 +10,7 @@ int main() {
 	cin >> n;
 
 	for (int a = 1; a <= n; a++) { //택희
-		for (int b = 1; b <= n; b++) { //영훈이
+		for (int b = 1; b <= n; b++) { //영훈
 			for (int c = 1; c <= n; c++) { //남규
 				if ((a + b + c == n) && (c >= b + 2) && (a % 2 == 0)) {
 					ret++;
