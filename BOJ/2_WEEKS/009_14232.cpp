64928a875bb149bcee80ea15e8b4674c5334e1ef
gyunnybot <idjkda4034@gmail.com>
2025-03-11 00:42:32 +0900

    Create 009_14232.cpp

---
+++ BOJ/2_WEEKS/009_14232.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+typedef long long ll;
+
+ll n;
+vector<ll> v;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	ll x = n;
+
+	for (ll i = 2; i * i <= n; i++) {
+		while (x % i == 0) {
+			v.push_back(i);
+			x /= i;			
+		}
+	}
+
+	if (x != 1) {
+		v.push_back(x);
+	}
+
+	cout << v.size() << '\n';
+	
+	for (ll i : v) {
+		cout << i << ' ';
+	}
+
+	return 0;
+}
