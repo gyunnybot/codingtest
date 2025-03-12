0756c8fd9d9b688b1a34be1052fe88cfdcede637
gyunnybot <idjkda4034@gmail.com>
2025-03-12 20:23:28 +0900

    Create 011_2247.cpp

---
+++ BOJ/2_WEEKS/011_2247.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int n, ret;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	for (int i = 2; i <= n; i++) {
+		int cnt = n / i; //n!에서 i의 배수는 몇 개인가?
+
+		ret += (cnt - 1) * i; //i 본인을 제외한 개수 x 가중치 i
+		ret %= 1000000; //mod 연산
+	}
+
+	cout << ret;
+
+	return 0;
+}
