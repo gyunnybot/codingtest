94e0b12ce2ac338cc0b552fc7e56e35fa96f3fb5
gyunnybot <idjkda4034@gmail.com>
2025-03-17 20:13:12 +0900

    COMMIT SQLS

---
+++ Programmers/SQL_Kit/06_STRING_DATE/009.sql
@@ -0,0 +1,10 @@
+SELECT U.USER_ID, U.NICKNAME, CONCAT(U.CITY, ' ', U.STREET_ADDRESS1, ' ', STREET_ADDRESS2) AS 전체주소, 
+CONCAT(SUBSTR(TLNO, 1, 3), '-', SUBSTR(TLNO, 4, 4), '-', SUBSTR(TLNO, 8)) AS 전화번호
+FROM USED_GOODS_USER AS U
+WHERE U.USER_ID IN (
+                    SELECT WRITER_ID
+                    FROM USED_GOODS_BOARD AS B
+                    GROUP BY WRITER_ID
+                    HAVING COUNT(*) >= 3
+                   )
+ORDER BY U.USER_ID DESC;
