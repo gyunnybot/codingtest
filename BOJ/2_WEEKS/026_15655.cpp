6b2399ad541a37ff5420550e67fcf6c0a3480a54
gyunnybot <idjkda4034@gmail.com>
2025-03-20 14:57:25 +0900

    ADD cpp codes

---
+++ BOJ/2_WEEKS/026_15655.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+#include<algorithm> //sort
+using namespace std;
+
+int n, m, a[10];
+vector<int> v;
+vector<bool> visited(10001, false);
+
+void print(vector<int>& v) {
+	for (int i : v) {
+		cout << i << ' ';
+	}
+
+	cout << '\n';
+
+	return;
+}
+
+void combi(int start, vector<int>& v, vector<bool>& visited) {
+	if (v.size() == m) {
+		print(v);
+		return;
+	}
+
+	for (int i = start + 1; i < n; i++) {
+		if (visited[i]) continue;
+
+		v.push_back(a[i]);
+		visited[i] = true;
+
+		combi(i, v, visited);
+
+		v.pop_back();
+		visited[i] = false;
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+
+	sort(a, a + n);
+
+	combi(-1, v, visited);
+
+	return 0;
+}
