233cba62363b4b79d431428080a62831f9f487c1
gyunnybot <idjkda4034@gmail.com>
2025-03-28 15:27:03 +0900

    UPDATE SQL ~group by

--- Programmers/SQL_Kit/02_SUM_MAX_MIN/010.sql
+++ Programmers/SQL_Kit/02_SUM_MAX_MIN/010.sql
@@ -4,7 +4,5 @@ FROM ECOLI_DATA AS A LEFT OUTER JOIN (
                                       FROM ECOLI_DATA
                                       GROUP BY Y
                                      ) AS B
-ON YEAR(A.DIFFERENTIATION_DATE) = B.Y -- WHERE, ON 절은 미완성 테이블로 가정. 속성에 대한 alias가 적용되지 않는다. (완성된 테이블 alias는 적용 가능)
-ORDER BY YEAR, YEAR_DEV; -- ORDER BY, GROUP BY 절은 테이블이 완성되었다고 가정. 속성의 alias가 적용된다
-
--- 정렬, 묶음에 관한 SQL 명령어와 그 하위 명령어는 이미 상위 테이블이 완성된 상태이므로 alias가 사용 가능하다
+ON YEAR(A.DIFFERENTIATION_DATE) = B.Y -- 조건절에서는 완성되지 않은 테이블 속성의 alias를 사용할 수 없다
+ORDER BY YEAR, YEAR_DEV;
