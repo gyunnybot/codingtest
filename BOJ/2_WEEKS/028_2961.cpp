d1acccbb318bf20702d91f82426e8242153f7ba9
gyunnybot <idjkda4034@gmail.com>
2025-04-04 18:46:47 +0900

    Create 028_2961.cpp

---
+++ BOJ/2_WEEKS/028_2961.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<algorithm> //sort
+#include<climits> //INT_MAX
+using namespace std;
+
+int n, s, b;
+int ret = INT_MAX;
+vector<pair<int, int>> ingre; //신 맛, 쓴 맛
+
+void recur(int idx, int sour, int bitter, int used) {
+	if (idx == n) {
+		if (used > 0) {
+			ret = min(ret, abs(sour - bitter));
+		}
+
+		return;
+	}
+
+	recur(idx + 1, sour * ingre[idx].first, bitter + ingre[idx].second, used + 1);
+	recur(idx + 1, sour, bitter, used);
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> s >> b;
+		ingre.push_back({ s,b });
+	}
+
+	recur(0, 1, 0, 0);
+
+	cout << ret;
+
+	return 0;
+}
