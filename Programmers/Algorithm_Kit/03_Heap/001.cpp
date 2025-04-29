36642beefacb8573f3014668a87219d1b3a59981
gyunnybot <idjkda4034@gmail.com>
2025-04-29 23:12:50 +0900

    Create 001.cpp

---
+++ Programmers/Algorithm_Kit/03_Heap/001.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <queue>
+#include <iostream>
+using namespace std;
+
+priority_queue<int,vector<int>,greater<int>> pq; //pq greater = 최소 힙
+
+int solution(vector<int> scoville, int K) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    int answer = 0;
+    
+    for(int sc : scoville) {
+        pq.push(sc);
+    }
+    
+    while(!pq.empty() && pq.top() < K) {
+        if(pq.size() >= 2) {
+            int temp = pq.top(); pq.pop();
+        
+            temp += 2 * pq.top(); pq.pop();
+            
+            pq.push(temp);
+            answer++;
+        }
+        else {
+            answer = -1;
+            break;
+        }
+    }
+    
+    return answer;
+}
