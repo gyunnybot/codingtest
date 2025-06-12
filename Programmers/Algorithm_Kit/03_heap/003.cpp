8b6184f01f0d8665f0ff82684f7f55b351de27e1
gyunnybot <idjkda4034@gmail.com>
2025-06-12 21:18:13 +0900

    Update 003.cpp

--- Programmers/Algorithm_Kit/03_heap/003.cpp
+++ Programmers/Algorithm_Kit/03_heap/003.cpp
@@ -9,37 +9,32 @@ multiset<int> mst;
 vector<int> solution(vector<string> operations) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
-
+    
     vector<int> answer;
-
-    for (string s : operations) {
-        if (s[0] == 'I') {
-            int num = atoi(s.substr(2).c_str());
-
-            mst.insert(num);
+    
+    for(string s : operations) {
+        if(s[0] == 'I') {
+            mst.insert(atoi(s.substr(2).c_str()));
         }
         else {
-            if (mst.empty()) continue;
-
-            int num = atoi(s.substr(2).c_str());
-
-            if (num == 1) {
+            if(mst.empty()) continue;
+            
+            if(atoi(s.substr(2).c_str()) == 1) {
                 mst.erase(prev(mst.end()));
             }
-            else if (num == -1) {
+            else {
                 mst.erase(mst.begin());
             }
         }
     }
-
-    if (mst.empty()) {
+    
+    if(mst.empty()) {
         answer.push_back(0);
         answer.push_back(0);
-    }
-    else {
+    } else {
         answer.push_back(*prev(mst.end()));
         answer.push_back(*mst.begin());
     }
-
+    
     return answer;
 }
