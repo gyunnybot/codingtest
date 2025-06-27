e4b091269e40a91356aba1e34433022ff3dfe12d
gyunnybot <idjkda4034@gmail.com>
2025-06-27 22:29:49 +0900

    Create 4 sql codes

---
+++ Programmers/SQL_Kit/01_SELECT/031.sql
@@ -0,0 +1,11 @@
+SELECT A.ID, CASE WHEN A.PER <= 0.25 THEN 'CRITICAL'
+                  WHEN A.PER <= 0.5 THEN 'HIGH'
+                  WHEN A.PER <= 0.75 THEN 'MEDIUM'
+                  ELSE 'LOW' END AS COLONY_NAME
+FROM (
+    SELECT ID, PERCENT_RANK() OVER (ORDER BY SIZE_OF_COLONY DESC) AS PER
+    FROM ECOLI_DATA
+) AS A
+ORDER BY A.ID;
+
+-- PERCENT_RANK() OVER (ORDER BY 속성 DESC) : 속성의 값들을 내림차순 정렬 후 비율 부여
