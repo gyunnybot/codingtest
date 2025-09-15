8d8c973654700f50088bea19b0e6a7ec08d5c806
gyunnybot <idjkda4034@gmail.com>
2025-09-15 21:06:57 +0900

    Update 015.sql

--- Programmers/SQL_Kit/03_GROUP_BY/015.sql
+++ Programmers/SQL_Kit/03_GROUP_BY/015.sql
@@ -1,9 +1,9 @@
-SET @hour := -1; -- SET @A := INTEGER -> INTEGER 타입의 변수 @A 선언
+SET @hour := -1; -- SET @A := val -> 변수 @A를 생성 후, val로 초기화
 
-SELECT (@hour := @hour + 1) AS HOUR, ( -- 0부터~
+SELECT (@hour := @hour + 1) AS HOUR, ( -- 할당 연산자. 변수 @hour에 (@hour + 1) 값을 넣고, 그 결과를 반환한다
     SELECT COUNT(*)
     FROM ANIMAL_OUTS
     WHERE HOUR(DATETIME) = @hour
 ) AS COUNT
 FROM ANIMAL_OUTS
-WHERE @hour < 23; -- 23까지
+WHERE @hour < 23; -- 23까지 실행 후 종료
