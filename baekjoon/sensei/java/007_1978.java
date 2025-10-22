7773949d941d8661813bc5f3dca07e2787563765
gyunnybot <idjkda4034@gmail.com>
2025-10-22 21:16:06 +0900

    create 007_1978.java

---
+++ baekjoon/sensei/java/007_1978.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, ret;
+    static boolean[] chae = new boolean[1001];
+
+    static void era(int num) {
+        chae[0] = false;
+        chae[1] = false;
+
+        for (int i = 2; i <= num; i++) {
+            chae[i] = true;
+        }
+
+        for (int i = 2; i <= num; i++) {
+            if (!chae[i]) continue;
+
+            for (int j = i + i; j <= num; j += i) {
+                chae[j] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        era(1000);
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            if (chae[Integer.parseInt(st.nextToken())]) {
+                ret++;
+            }
+        }
+
+        System.out.println(ret);
+    }
+}
