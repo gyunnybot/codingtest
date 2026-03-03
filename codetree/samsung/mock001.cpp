075149449634fa87bd1e42af5aaa28e12d7dc025
gyunnybot <gyunnybot4029@gmail.com>
2026-03-03 19:56:50 +0900

    docs: 포매팅 수정

--- codetree/samsung/mock001.cpp
+++ codetree/samsung/mock001.cpp
@@ -1,33 +1,38 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 typedef unsigned long long ll;
 
 int n, a[1000001], leader, teammate;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
 
-	cin >> n; // 식당의 수
+  cin >> n; // 식당 수
 
-	for (int i = 0; i < n; i++) {
-		cin >> a[i]; // 고객의 수
-	}
+  for (int i = 0; i < n; i++) {
+    cin >> a[i]; // 고객 수
+  }
 
-	cin >> leader >> teammate; // 팀장 최대 할당량, 팀원 최대 할당량
+  cin >> leader >> teammate; // 팀장 최대 할당량, 팀원 최대 할당량
 
-	ll ret = n;
-	
-	for (int i = 0; i < n; i++) {
-		if (a[i] <= leader) continue;
+  ll ret = n;
 
-		a[i] -= leader;
-		ret += a[i] / teammate;
+  for (int i = 0; i < n; i++) {
+    if (a[i] <= leader) {
+      continue;
+    }
 
-		if (a[i] % teammate) ret += 1;
-	}
+    a[i] -= leader;
+    ret += a[i] / teammate;
 
-	cout << ret;
+    if (a[i] % teammate) {
+      ret += 1;
+    }
+  }
 
-	return 0;
+  cout << ret;
+
+  return 0;
 }
