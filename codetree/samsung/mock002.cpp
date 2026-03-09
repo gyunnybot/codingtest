432f50a2e96d4565e3af16a9d26f15605843bc95
gyunnybot <gyunnybot4029@gmail.com>
2026-03-09 10:46:49 +0900

    refactor: 주석 추가

--- codetree/samsung/mock002.cpp
+++ codetree/samsung/mock002.cpp
@@ -1,3 +1,17 @@
+/*
+삼성 2015 하반기 2번 문제 - 2개의 사탕
+
+목적지로 이동해야 큐가 끝나는 것이 아니라, 해당 좌표가 정답인지 확인하는 유형
+
+ny를 바로 cur.y + dy[i]로 박아버리면 cur.y 자체가 정답인 경우를 무시하게 되어 최소 횟수가 안나오게 됨
+
+그럼 앞으로 모두 ny = cur.y? 문제마다 다른 것 같다
+
+출발점이 있고 목적지가 있다면 ny = cur.y + dy[i], 현재 좌표가 조건에 맞는지 체크하는 경우 ny = cur.y
+
+시간복잡도 O((N*M)^2)
+*/
+
 #include <iostream>
 #include <queue>
 using namespace std;
@@ -9,21 +23,21 @@ int n, m, ret;
 char a[11][11];
 string s;
 
-struct info {
+struct Info {
   int ry, rx, by, bx, cnt;
 };
-info start;
+Info start;
 
 int bfs() {
   visited[start.ry][start.rx][start.by][start.bx] = 1;
 
-  queue<info> q;
+  queue<Info> q;
   q.push(start);
 
   int temp = -1;
 
   while (!q.empty()) {
-    info cur = q.front(); q.pop();
+    Info cur = q.front(); q.pop();
 
     if (cur.cnt > 10) {
       break;
@@ -81,8 +95,8 @@ int bfs() {
             nbx -= dx[i];
           }
         }
-        
-        // 겹치면서 출구인 경우, 더 이상 queue에 들어갈 필요가 없다
+
+        // 겹치면서 출구인 경우는 더 이상 queue에 들어갈 필요가 없다
       }
 
       if (!visited[nry][nrx][nby][nbx]) {
