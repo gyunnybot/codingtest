41b12e3ddd0836d6a65ca2cf70fd36b9fb1a4c96
gyunnybot <gyunnybot4029@gmail.com>
2026-05-27 09:22:00 +0900

    refactor: 폰켓몬

--- programmers/algorithmkit/01_hash/002.java
+++ programmers/algorithmkit/01_hash/002.java
@@ -6,8 +6,8 @@ class Solution {
         
         Set<Integer> st = new HashSet<>();
         
-        for(int i : nums) {
-            st.add(i);
+        for(int num : nums) {
+            st.add(num);
         }
         
         answer = Math.min(st.size(), nums.length / 2);
@@ -18,14 +18,10 @@ class Solution {
 
 /**
  * Set : add
- * set은 hash 기반 + key-value가 아니라 get이 없다. 대신 .contains(value)
+ * set의 경우 해시 기반이라 get이 없다. 대신 .contains(value) 사용
  * 
- * 길이 반환
+ * 크기 반환
  * 기본 배열 [] : .length
  * 문자열 String : .length()
  * 컬렉션 List, Set, Map : size()
- * 
- * 비어있는지 확인할 때
- * 기본 배열 [] : .length == 0
- * 문자열, 컬랙션 : .isEmpty(), .size() == 0
  */
