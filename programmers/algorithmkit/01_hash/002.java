d1ef9d5730c8af3ee72b955e425a56ddc8c846c4
gyunnybot <gyunnybot4029@gmail.com>
2026-05-28 11:45:59 +0900

    refactor: 변수명, 주석 수정

--- programmers/algorithmkit/01_hash/002.java
+++ programmers/algorithmkit/01_hash/002.java
@@ -20,8 +20,13 @@ class Solution {
  * Set : add
  * set의 경우 해시 기반이라 get이 없다. 대신 .contains(value) 사용
  * 
- * 크기 반환
+ * 크기 반환 :
  * 기본 배열 [] : .length
  * 문자열 String : .length()
  * 컬렉션 List, Set, Map : size()
+ * 
+ * 정석적인 컬랙션 공백 체크 :
+ * if (collection != null && !collection.isEmpty()) {
+ *   // 컬렉션이 null이 아니고 비어있지 않을 때 로직 수행
+ * }
  */
--- programmers/algorithmkit/02_stack_queue/001.java
+++ programmers/algorithmkit/02_stack_queue/001.java
@@ -2,21 +2,21 @@ import java.util.*;
 
 public class Solution {
     public List<Integer> solution(int[] arr) {
-        List<Integer> temp = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();
         
         int val = -1; // 배열 arr의 각 원소는 숫자 0부터 9까지로 이루어져 있습니다.
         
         for(int i : arr) {
             if(i != val) {
-                temp.add(i);
+                answer.add(i);
                 val = i;
             }
         }
         
-        return temp;
+        return answer;
     }
 }
 
 /**
- * 반환형 까다로우면 바꿔도 된다!
+ * 반환형이 맘에 안들면 바꿔도 된다. 출력만 잘 된다면 가능
  */
